97a3351486a3250c2c50e36c6fc3f0c7c2906eac
허세진 <heosejin222@gmail.com>
2024-09-12 20:54:51 +0900

    dribbble3(컬러변경 추가)

--- dribbble/dribbble.js
+++ dribbble/dribbble.js
@@ -3,3 +3,16 @@ document.querySelectorAll('.position-left').forEach(element=>
         const leftValue = element.getAttribute('data-left');
         element.style.left = leftValue;
     })
+    const notice = document.querySelector('.Notice');
+let hue = 0;
+
+function changeGradient() 
+{
+    hue += 0.1;
+    const gradient = `linear-gradient(90deg, hsl(${hue}, 100%, 70%), hsl(${hue + 1}, 100%, 70%))`;
+    //linear-gradient = css함수
+    notice.style.background = gradient;
+
+    requestAnimationFrame(changeGradient);
+}
+changeGradient();
